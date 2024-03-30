71a7cf7aea5560dc583927b0975a17f032934983
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-30 16:39:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 #
-#Copyright 2021-2024 yangxu52<https://github.com/yangxu52>
-#
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
-#
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
 #Unless required by applicable law or agreed to in writing, software
@@ -13,6 +7,7 @@
 #WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #See the License for the specific language governing permissions and
 #limitations under the License.
+
 
 echo '替换jerrykuku的luci argon主题'
 #rm -rf feeds/luci/themes/luci-theme-argon
@@ -38,3 +33,6 @@ echo '=========ALTER passwall denpendcies check OK!========='
 # echo '开启sing-box的CGO标记'
 # sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/passwall/sing-box/Makefile
 # echo '=========Enable sing-box CGO FLAG OK !========='
+
+
+
