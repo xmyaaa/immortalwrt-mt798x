1ebf2fe0b30a3c42326e5e3ad820a4b5ef7dad9c
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-30 13:15:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+#Copyright 2021-2024 yangxu52<https://github.com/yangxu52>
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
 
 echo '替换jerrykuku的luci argon主题'
 #rm -rf feeds/luci/themes/luci-theme-argon
@@ -30,20 +25,15 @@ git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang 
 echo '=========Replace golang OK!========='
 
 echo '替换Passwall软件'
-#rm -rf feeds/luci/applications/luci-app-passwall
-#git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
-#mv feeds/luci/applications/luci-app-passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
-#rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
+mv feeds/luci/applications/luci-app-passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
+rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
 echo '=========Replace passwall source OK!========='
 
 echo '修改Passwall检测规则'
 #sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/app.sh
 echo '=========ALTER passwall denpendcies check OK!========='
-
-echo '修改主机名'
-sed -i "s/hostname='ImmortalWrt'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep hostname=
-echo '=========Alert hostname OK!========='
 
 # echo '开启sing-box的CGO标记'
 # sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/passwall/sing-box/Makefile
