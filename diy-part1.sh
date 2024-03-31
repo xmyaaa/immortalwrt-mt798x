45e80693648f0c33d84070cb8a1546f631bf9034
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-31 23:23:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,3 @@ sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
 # echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo '=========Add passwall feeds source OK!========='
-
-# Add a feed source
-# rm -rf package/helloworld
-# git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
