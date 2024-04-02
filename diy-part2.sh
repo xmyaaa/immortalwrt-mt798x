90a035d077f4b92fbd7beac19fb774b97f19528c
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-02 22:36:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@
 
 echo '替换jerrykuku的luci argon主题'
 # rm -rf feeds/luci/themes/luci-theme-argon
-#git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
+# git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 echo '=========Replace luci theme argon OK!========='
 
 echo '替换golang到1.22.x'
@@ -25,23 +25,24 @@ git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang 
 echo '=========Replace golang OK!========='
 
 echo '替换Passwall软件'
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
-mv feeds/luci/applications/luci-app-passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
-rm -rf feeds/luci/applications/luci-app-passwall/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall/*
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
+mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
+rm -rf passwall
+echo '=========Replace passwall source OK!========='
+
+echo '添加Passwall2软件'
+# rm -rf feeds/luci/applications/luci-app-passwall2
+#mkdir feeds/luci/applications/luci-app-passwall2
+#git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2 passwall2
+#mv passwall2/luci-app-passwall2/* feeds/luci/applications/luci-app-passwall2/
+# rm -rf passwall2
 echo '=========Replace passwall source OK!========='
 
 echo '修改Passwall检测规则'
 sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/app.sh
 echo '=========ALTER passwall denpendcies check OK!========='
 
-# echo '开启sing-box的CGO标记'
-# sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/passwall/sing-box/Makefile
-# echo '=========Enable sing-box CGO FLAG OK !========='
-
-echo '修改默认IP'
-sed -i 's/ipad=${ipaddr:-"192.168.1.1"}/ipad=${ipaddr:-"192.168.10.1"}/g' package/base-files/files/bin/config_generate
-sed -i 's/addr_offset=2/addr_offset=8/g' package/base-files/files/bin/config_generate
-sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).1"}/${ipaddr:-"192.168.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep hostname=
-echo '=========Alert Default IP OK!========='
+echo '修改Passwall2检测规则'
+#sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall2/root/usr/share/passwall2/app.sh
+echo '=========ALTER passwall denpendcies check OK!========='
