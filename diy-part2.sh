2d35536db6e7bc546e0e307d1231d96d3aab6f0e
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-04 08:12:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,16 +25,16 @@ git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang 
 echo '=========Replace golang OK!========='
 
 echo '替换Passwall软件'
-rm -rf feeds/luci/applications/luci-app-passwall/*
-git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
-mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
-rm -rf passwall
+# rm -rf feeds/luci/applications/luci-app-passwall/*
+#git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
+#mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
+# rm -rf passwall
 echo '=========Replace passwall source OK!========='
 
 echo '添加Passwall2软件'
 # rm -rf feeds/luci/applications/luci-app-passwall2
-mkdir feeds/luci/applications/luci-app-passwall2
-git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2 passwall2
+#mkdir feeds/luci/applications/luci-app-passwall2
+#git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2 passwall2
 mv passwall2/luci-app-passwall2/* feeds/luci/applications/luci-app-passwall2/
 rm -rf passwall2
 echo '=========Replace passwall source OK!========='
